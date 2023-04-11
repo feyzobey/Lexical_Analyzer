dddfc4739dae1c397a0d5ab36d4d9f700d0a3554
FaBeY34 <feyzullahasillioglu@marun.edu.tr>
2023-04-11 03:35:03 +0300

    brackets added and toString added

--- src/lexicalAnalyzer.java
+++ src/lexicalAnalyzer.java
@@ -15,61 +15,96 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class lexicalAnalyzer {
-
 	// global variables to trace chars and tokens
-	static int lineNo = 0;
-	static int columnNo = 0;
+	static int lineNo = 1;
+	static int columnNo = 1;
 	static String token = "";
-	
+
 	public static void main(String[] args) throws IOException {
 
 		System.out.println("Enter the name of the input file: ");
 		Scanner scanFileName = new Scanner(System.in);
 		String input = scanFileName.nextLine();
-		System.out.println(input);
 		scanFileName.close();
-		
+
 		File inputFile = new File(input);
 		FileReader fileReader = new FileReader(inputFile);
 		// read char by char. Reads characters from another Reader
 		BufferedReader readChar = new BufferedReader(fileReader);
-		
 		int ascii = 0;
 		ascii = readChar.read();
 		// -1 means end of character stream
 		while (ascii != -1) {
+			// ascii 10 is new line character
+			if (ascii == 10) {
+				lineNo++;
+				columnNo = 1;
+				continue;
+			}
+			// cast to char
 			char ch = (char) ascii;
-			System.out.print(ch);
+			// if char is a bracket
+			if (isBracket(ch)) {
+				// if char is a left bracket
+				if (ch == '(') {
+					token = "LEFTPAR";
+				} else if (ch == ')') {
+					token = "RIGHTPAR";
+				} else if (ch == '[') {
+					token = "LEFTSQUAREB";
+				} else if (ch == ']') {
+					token = "RIGHTSQUAREB";
+				} else if (ch == '{') {
+					token = "LEFTCURLYB";
+				} else if (ch == '}') {
+					token = "RIGHTCURLYB";
+				}
+			} else {
+				token = "LEXICAL ERROR";
+			}
+			System.out.println(ch);
+			System.out.println(toString(token, lineNo, columnNo));
 			ascii = readChar.read();
 		}
+		readChar.close();
 	}
-	
-//	public boolean isBracket(char c) {
-//
-//	}
-//
-//	public boolean isNumber(char c) {
-//
-//	}
-//
-//	public boolean isBoolean(String s) {
-//
-//	}
-//
-//	public boolean isCharacter(char c) {
-//
-//	}
-//
-//	public boolean isString(String s) {
-//
-//	}
-//
-//	public boolean isKeyword(String s) {
-//
-//	}
-//
-//	public boolean isIdentifier(String s) {
-//
-//	}
 
+	public static boolean isNumber(char c) {
+		if (Character.isDigit(c)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isBracket(char c) {
+		Character ch = (Character) c;
+		if (ch.equals('(') || ch.equals(')') || ch.equals('[') || ch.equals(']') || ch.equals('{') || ch.equals('}')) {
+			return true;
+		}
+		return false;
+	}
+
+	// public static boolean isBoolean(String s) {
+
+	// }
+
+	// public static boolean isCharacter(char c) {
+
+	// }
+
+	// public static boolean isString(String s) {
+
+	// }
+
+	// public static boolean isKeyword(String s) {
+
+	// }
+
+	// public static boolean isIdentifier(String s) {
+
+	// }
+
+	public static String toString(String token, int lineNo, int columnNo) {
+		return token + " " + lineNo + ":" + columnNo;
+	}
 }
